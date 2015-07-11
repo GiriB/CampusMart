2b2cc3736dbfe4c21e9da3b052dc6805e1c507c4
Giri Gaurav Bhatnagar <giri1gb@gmail.com>
2015-07-12 01:08:52 +0530

    Pull information from ISBN number using Google Books API

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@
 /*
  * GET home page.
  */
+var https = require('https'); 
 var ItemSchema = require('../schema/item');
+
+//HIDE THIS LATER 
+var BOOK_API_KEY = 'AIzaSyC7bh-uEk_-llRiraASNDqjK5MCgnSDntg';
 
 exports.buysell = function(req, res){
   res.render('index', { title: 'CampusMart' });
@@ -36,7 +40,7 @@ exports.search = function (req,res){
 };
 
 exports.buy = function(req,res){
-res.render('buy',{});
+	res.render('buy',{});
 
 };
 
@@ -64,30 +68,73 @@ exports.list = function(req,res){
 exports.save = function(req,res){
 
 	//authentication to be done here 
+	
+
+
+
+
+	
+	// GET https://www.googleapis.com/books/v1/volumes?q=9788129711960&key={YOUR_API_KEY}
+	//GET https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=yourAPIKey
+
+
+	var path = "/books/v1/volumes?q=+isbn:"+req.body.isbn;
+	
+	path=path+'&key='+BOOK_API_KEY;
+	console.log(path)
+
+	var options = {
+  		host: 'www.googleapis.com',
+  		path: path,
+	};
+	
+	// console.log(path+"is the link generated for the given ISBN");
+	var str ='';
+	var objectJSON;
+	//API that gets ISBN data 
+	https.request(options, function(res){
+		res.on('data', function (chunk) {
+    	str += chunk;
+  		});
+
+    //the whole response has been recieved, so we just print it out here
+  		res.on('end', function () {
+    	console.log(str);
+    	objectJSON=JSON.parse(str);
+  		});
+	}).end();
+
+
+
+
+	
+
 	//create item object with received values
- var record = new ItemSchema({created:Date.now(),
-          ISBN:req.body.isbn,
-          collegeIDs:req.body.collegeID,
-          sellerEmail:req.body.email});
- //save the instance
- record.save(function(err){
- 	if(err){
- 		console.log(err);
- 		res.status(500).json({status:'failure'});
- 	}
- });
+	// var record = new ItemSchema({created:Date.now(),
+ //          ISBN:req.body.isbn,
+ //          collegeIDs:req.body.collegeID,
+ //          sellerEmail:req.body.email});
+ 	
+ // 	//save the instance
+	// record.save(function(err){
+	//  	if(err){
+	//  		console.log(err);
+	//  		res.status(500).json({status:'failure'});
+	//  	}
+ // 	});
 
 
-  console.log("The details are : ");
-  console.log(record['name']);
-  console.log(record['sellerEmail']);
-  console.log(record['created']);
-  console.log(record['collegeIDs']);
-  
-   // sends a JSON object
-  // -- res.json(req.body);
-  //con verts the object into JSON string 
-  var details = JSON.stringify(req.body);
-  res.json({status:'done'});
+	// console.log("The details are : ");
+	// console.log(record['name']);
+	// console.log(record['sellerEmail']);
+	// console.log(record['created']);
+	// console.log(record['collegeIDs']);
+
+	// sends a JSON object
+	// -- res.json(req.body);
+	//con verts the object into JSON string 
+	var details = JSON.stringify(req.body);
+	res.json({status:'done'});
+	console.log('At the end');
   
 };
