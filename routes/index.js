6898bd58cac2f383babc71a5a519e0021c85a3b8
Giri Gaurav Bhatnagar <giri1gb@gmail.com>
2015-07-16 21:15:00 +0530

    new package.json and index.js

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,14 @@
  * GET home page.
  */
 var https = require('https'); 
+var config = require('../config');
 var ItemSchema = require('../schema/item');
+var myModules = require('../myModules/myModules');
 
 //HIDE THIS LATER 
-var BOOK_API_KEY = 'AIzaSyC7bh-uEk_-llRiraASNDqjK5MCgnSDntg';
+var BOOK_API_KEY = config.BOOK_API_KEY;
+console.log(config.BOOK_API_KEY);
+
 
 exports.buysell = function(req, res){
   res.render('index', { title: 'CampusMart' });
@@ -65,6 +69,11 @@ exports.list = function(req,res){
 
 }
 
+
+exports.saveManual = function(req,res){
+	res.send('<h3>Save Manual</h3>');
+}
+
 exports.save = function(req,res){
 
 	//authentication to be done here 
@@ -76,38 +85,47 @@ exports.save = function(req,res){
 	
 	// GET https://www.googleapis.com/books/v1/volumes?q=9788129711960&key={YOUR_API_KEY}
 	//GET https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=yourAPIKey
-
-
-	var path = "/books/v1/volumes?q=+isbn:"+req.body.isbn;
-	
+	var path = "/books/v1/volumes?q=isbn:"+req.body.isbn;
 	path=path+'&key='+BOOK_API_KEY;
-	console.log(path)
-
 	var options = {
   		host: 'www.googleapis.com',
   		path: path,
 	};
 	
-	// console.log(path+"is the link generated for the given ISBN");
-	var str ='';
-	var objectJSON;
+	var str =''; //data we are receiving
+	var objJSON; // json object that we create from the response we get 
+
 	//API that gets ISBN data 
-	https.request(options, function(res){
-		res.on('data', function (chunk) {
+	https.request(options, function(response){
+		response.on('data', function (chunk) {
     	str += chunk;
   		});
 
-    //the whole response has been recieved, so we just print it out here
-  		res.on('end', function () {
-    	console.log(str);
-    	objectJSON=JSON.parse(str);
+  		response.on('end', function () {
+    		objJson=JSON.parse(str);
+	        if(objJson.hasOwnProperty('items')){
+	    	    //if we got a valid response , we can save here !
+
+
+	    	    //pass the details about the book
+	    		//res.render('save',{status:"autoSave"});
+	    		res.render('save',{status:"autoSave"});
+	    	}else{
+	    		//if we get an empty response ,we have to ask details about the book 
+	    		//  redirect to a form that gets the details about the obook and then saves.
+
+	    		//redirect to manually enter the details of the books
+	    		//res.send("<h3>Can't locate the book, enter details manually!!</h3>");
+	    		res.render('save',{status:"manualSave"});
+
+	    	}
   		});
 	}).end();
 
 
 
 
-	
+	//collegeId's can be more than one !!
 
 	//create item object with received values
 	// var record = new ItemSchema({created:Date.now(),
@@ -133,8 +151,8 @@ exports.save = function(req,res){
 	// sends a JSON object
 	// -- res.json(req.body);
 	//con verts the object into JSON string 
-	var details = JSON.stringify(req.body);
-	res.json({status:'done'});
+	//var details = JSON.stringify(req.body);
+	//res.json({status:'done'});
 	console.log('At the end');
   
 };
